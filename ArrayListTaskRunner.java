bd5bad6a66b7df447fef0d66040b01dd6e019521
Narmadha154 <85552355+Narmadha154@users.noreply.github.com>
2021-08-19 17:02:27 +0530

    Add files via upload

--- ArrayListTaskRunner.java
+++ ArrayListTaskRunner.java
@@ -1,137 +1,6 @@
-package task4;
+
 import java.util.*;
- public class ArrayListTaskRunner{
-    public static int findLength(){
-      ArrayList<String> list=new ArrayList<>();
-      int length=list.size();
-      return length;
-    }
-    public static ArrayList<Object> createList(ArrayList<Integer> list,ArrayList<String> stringList,ArrayList<Student> customArrList){
-       ArrayList<Object> arrList=new ArrayList<>();
-       arrList.addAll(list);
-       arrList.addAll(stringList);
-       arrList.addAll(customArrList);
-      return arrList;
-    }
-     public static ArrayList<String> findIndex(ArrayList<String> list,String str){
-      int length=list.size();
-      int index=list.indexOf(str);
-      System.out.println("Index of the element is:"+index);
-      System.out.println("Length of the ArrayList :"+length);
-      return list;
-    }
-   public static ArrayList<String> findStringAtIndex(ArrayList<String> list,int index){
-      String str=list.get(index);
-      System.out.println("String at the given index is:"+str);
-      return list;
-    }
-   public static int findIndexOfDuplicate(ArrayList<String> list){
-      String str=null;
-      for(int i=0;i<list.size();i++){
-         for(int j=i+1;j<list.size();j++){
-            if(list.get(i).equals(list.get(j))){
-               str=list.get(j);
-            }
-         }
-      }
-      int firstIndex=list.indexOf(str);
-      System.out.println("First index of the duplicate element is:"+firstIndex);
-      int lastIndex=list.lastIndexOf(str);
-      return lastIndex;
-    }
-   public static ArrayList<String> addString(ArrayList<String> list,String str,int position){
-      list.add(position,str);
-      return list;
-    }
-     public static ArrayList<String> createArrayList(ArrayList<String> list,int startIndex,int endIndex){
-      ArrayList<String> secondList=new ArrayList<>();
-      for(int i=startIndex;i<endIndex;i++){
-           secondList.add(list.get(i));
-      }
-      return secondList;
-    }
-   public static ArrayList<String> createThreeArrayList(ArrayList<String> firstList){
-      ArrayList<String> secondList=ArrayListTaskRunner.createStringArrayList();
-      ArrayList<String> thirdList=new ArrayList<String>();
-      thirdList.addAll(firstList);
-      thirdList.addAll(secondList);
-      return thirdList;
-    }
-   public static ArrayList<String> createThirdArrayList(ArrayList<String> firstList){
-     ArrayList<String> secondList=ArrayListTaskRunner.createStringArrayList();
-      ArrayList<String> thirdList=new ArrayList<String>();
-      thirdList.addAll(secondList);
-      thirdList.addAll(firstList);
-      return thirdList;
-    }
-   public static ArrayList<Double> removeDecimalValue(ArrayList<Double> decimalList,double decimalValue){
-      decimalList.remove(decimalValue);
-      return decimalList;
-    }   
-   public static ArrayList<Double> removeDecimalValueAtIndex(ArrayList<Double> decimalList,int position){
-      decimalList.remove(position);
-      return decimalList;
-    } 
-   public static ArrayList<String> removeElements(ArrayList<String> firstList){
-      ArrayList<String> secondList=ArrayListTaskRunner.createStringArrayList();
-      firstList.removeAll(secondList);
-      return firstList;
-    }
-   public static ArrayList<String> removeNonDuplicateElements(ArrayList<String> firstList){
-      ArrayList<String> secondList=ArrayListTaskRunner.createStringArrayList();
-      firstList.retainAll(secondList);
-      return firstList;
-    }
-  public static ArrayList<Long> removeAllLongValue(ArrayList<Long> longList){
-      longList.removeAll(longList);
-      return longList;
-    }
-  public static ArrayList<String> checkString(ArrayList<String> list,String str){
-       if(list.contains(str)){
-            System.out.println("Given string is present in the ArrayList");
-       }
-      return list;
-    } 
-   public static ArrayList<Long> removeLongValue(ArrayList<Long> longList,int startIndex,int endIndex){
-        longList.removeRange(startIndex,endIndex);
-   return longList;
-}
-  /*class Remove extends ArrayList<Long>{
-      public static  ArrayList<Long> removeLongValue(){
-         Remove longList=new Remove();
-         Scanner sc=new Scanner(System.in);
-         System.out.println("Enter the no.of long values:");
-         int size1=sc.nextInt();
-         System.out.println("Enter the long values:");
-         Long [] longArray=new Long[size1];
-         for(int i=0;i<size1;i++){
-            longArray[i]=sc.nextLong();
-         }
-        for(int i=0;i<longArray.length;i++){
-          longList.add(longArray[i]);
-        } 
-        System.out.println("Enter the start index:");
-        int startIndex=sc.nextInt();
-        System.out.println("Enter the end index:");
-        int endIndex=sc.nextInt();
-        longList.removeRange(startIndex,endIndex);
-      return longList;
-    } 
-  }
-    class Student{
-       int rollNo;
-       String name;
-       int marks;
-       Student(int rollNo,String name,int marks){
-          this.rollNo=rollNo;
-          this.name=name;
-          this.marks=marks;
-      }
-      public String toString(){
-           return "RollNo: "+rollNo+" "+"Name: "+name+" "+"Marks: "+marks;
-      }
-   }*/
-//public class ArrayListTaskRunner{
+public class ArrayListTaskRunner{
    public static ArrayList<String> createStringArrayList(){
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter the number of Strings:");
@@ -215,7 +84,7 @@ import java.util.*;
     int number=sc.nextInt();
     switch(number){
     case 1:
-       int length=findLength();
+       int length=ArrayListTask.findLength();
        System.out.println("Length of the ArrayList is:"+length);
        break;
    case 2:
@@ -249,19 +118,19 @@ import java.util.*;
         ArrayList<String> listArray=createStringArrayList();
         ArrayList<Integer> listInteger=createIntegerArrayList();
          ArrayList<Student> customArrList=createCustomList();
-       ArrayList<Object> objList=createList(listInteger,listArray,customArrList);
+       ArrayList<Object> objList=ArrayListTask.createList(listInteger,listArray,customArrList);
        System.out.println("ArrayList elements are:");
        for(int i=0;i<objList.size();i++){
          System.out.println(objList.get(i)+" ");
       }
-      int objLength=objList.size();
+       int objLength=objList.size();
        System.out.println("ArrayList size:"+objLength);
        break;
   case 6:
        ArrayList<String> list3=createStringArrayList();
        System.out.println("Enter the string for which you want to find index:");
        String str=sc.next();
-       ArrayList<String> strList=findIndex(list3,str);
+       ArrayList<String> strList=ArrayListTask.findIndex(list3,str);
        System.out.println("ArrayList elements are:");
        for(int i=0;i<strList.size();i++){
          System.out.println(strList.get(i));
@@ -284,13 +153,13 @@ import java.util.*;
        ArrayList<String> list5=createStringArrayList();
        System.out.println("Enter the index:");
        int index=sc.nextInt();
-       ArrayList<String> newList=findStringAtIndex(list5,index);
+       ArrayList<String> newList=ArrayListTask.findStringAtIndex(list5,index);
        int newLength=newList.size();
        System.out.println("Length of the string :"+newLength);
        break;
   case 9:
        ArrayList<String> list6=createStringArrayList();
-       int lastIndex=findIndexOfDuplicate(list6);
+       int lastIndex=ArrayListTask.findIndexOfDuplicate(list6);
        System.out.println("Last index of the duplicate element is:"+lastIndex);
        break;
   case 10:
@@ -299,7 +168,7 @@ import java.util.*;
        String strSecond=sc.next();
        System.out.println("Enter the position where string to be entered:");
        int position=sc.nextInt();
-       ArrayList<String> addedList=addString(list7,strSecond,position);
+       ArrayList<String> addedList=ArrayListTask.addString(list7,strSecond,position);
        System.out.println("ArrayList elements after adding a string:");
        for(String arr:addedList){
          System.out.println(arr);
@@ -313,7 +182,7 @@ import java.util.*;
        int startIndex=sc.nextInt();
        System.out.println("Enter the index where second arraylist ends:");
        int endIndex=sc.nextInt();
-       ArrayList<String> secondList=createArrayList(list8,startIndex,endIndex);
+       ArrayList<String> secondList=ArrayListTask.createArrayList(list8,startIndex,endIndex);
        System.out.println("Second ArrayList elements:");
        for(String secondArray:secondList){
          System.out.println(secondArray);
@@ -323,7 +192,8 @@ import java.util.*;
        break;
  case 12:
        ArrayList<String> list9=createStringArrayList();
-       ArrayList<String> thirdList=createThreeArrayList(list9);
+       ArrayList<String> secondList2=createStringArrayList();
+       ArrayList<String> thirdList=ArrayListTask.createThreeArrayList(list9,secondList2);
        System.out.println("Third ArrayList elements:");
       for(int i=0;i<thirdList.size();i++){
          System.out.println(thirdList.get(i)+" ");
@@ -333,7 +203,8 @@ import java.util.*;
        break;
   case 13:
        ArrayList<String> list10=createStringArrayList();
-       ArrayList<String> thirdArrList=createThirdArrayList(list10);
+       ArrayList<String> secondArrList=createStringArrayList();
+       ArrayList<String> thirdArrList=ArrayListTask.createThirdArrayList(list10,secondArrList);
        System.out.println("Third ArrayList elements:");
        for(int i=0;i<thirdArrList.size();i++){
          System.out.println(thirdArrList.get(i)+" ");
@@ -345,7 +216,7 @@ import java.util.*;
        ArrayList<Double> doubleList=createDecimalArrayList();
        System.out.println("enter the value to be removed:");
        double decimalValue=sc.nextDouble();
-       ArrayList<Double> decimalList=removeDecimalValue(doubleList,decimalValue);
+       ArrayList<Double> decimalList=ArrayListTask.removeDecimalValue(doubleList,decimalValue);
        int decimalListLength=decimalList.size();
        System.out.println("Arraylist after removing an element:");
        for(Double arr:decimalList){
@@ -357,7 +228,7 @@ import java.util.*;
        ArrayList<Double> doubleList1=createDecimalArrayList();
        System.out.println("Enter the index where the value to be removed:");
        int removePosition=sc.nextInt();
-       ArrayList<Double> deciList=removeDecimalValueAtIndex(doubleList1,removePosition);
+       ArrayList<Double> deciList=ArrayListTask.removeDecimalValueAtIndex(doubleList1,removePosition);
        int deciLength=deciList.size();
        System.out.println("Arraylist after removing an element at given position:");
        for(Double deciArray:deciList){
@@ -366,12 +237,12 @@ import java.util.*;
        System.out.println("ArrayList size after removing an element:"+deciLength);
        break;
  case 16:
-       ArrayList<Long> longArrayList=createLongArrayList();
+        ArrayList<Long> longList1=createLongArrayList();
        System.out.println("Enter the start index:");
-        int startIndexNew=sc.nextInt();
-        System.out.println("Enter the end index:");
-        int endIndexNew=sc.nextInt();
-       ArrayList<Long> longArrayList1=removeLongValue(longArrayList,startIndexNew,endIndexNew);
+       int stIndex=sc.nextInt();
+       System.out.println("Enter the end index:");
+       int edIndex=sc.nextInt();
+       ArrayList<Long> longArrayList=ArrayListTask.removeLongValue(longList1,stIndex,edIndex);
        int removedListLength=longArrayList.size();
         System.out.println("Arraylist after removing the elements:");
         for(Long arrayLong:longArrayList){
@@ -381,7 +252,7 @@ import java.util.*;
        break;
  case 17:
        ArrayList<String> list11=createStringArrayList();
-       ArrayList<String> firstList=removeElements(list11);
+       ArrayList<String> firstList=ArrayListTask.removeElements(list11);
        System.out.println("First ArrayList elements after removing elements which are present in 2nd arraylist:");
        for(int i=0;i<firstList.size();i++){
          System.out.println(firstList.get(i)+" ");
@@ -391,7 +262,7 @@ import java.util.*;
        break;
  case 18:
        ArrayList<String> list12=createStringArrayList();
-       ArrayList<String> firstArrList=removeNonDuplicateElements(list12);
+       ArrayList<String> firstArrList=ArrayListTask.removeNonDuplicateElements(list12);
        System.out.println("First ArrayList elements after removing elements which are not present in 2nd arraylist:");
        for(int i=0;i<firstArrList.size();i++){
          System.out.println(firstArrList.get(i)+" ");
@@ -401,7 +272,7 @@ import java.util.*;
        break;
   case 19:
        ArrayList<Long> longList=createLongArrayList();
-       ArrayList<Long> longArrList=removeAllLongValue(longList);
+       ArrayList<Long> longArrList=ArrayListTask.removeAllLongValue(longList);
        int longListSize=longArrList.size();
        System.out.println("Arraylist after removing the elements:");
        for(Long longArr:longList){
@@ -413,7 +284,7 @@ import java.util.*;
        ArrayList<String> list13=createStringArrayList();
        System.out.println("Enter the string to be checked:");
        String checkStr=sc.next();
-       ArrayList<String> checkList=checkString(list13,checkStr);
+       ArrayList<String> checkList=ArrayListTask.checkString(list13,checkStr);
        int checkListLength=checkList.size();
        System.out.println("ArrayList elements are:");
        for(String checkArr:checkList){
@@ -427,17 +298,4 @@ import java.util.*;
    }
  }
 }
-    class Student{
-       int rollNo;
-       String name;
-       int marks;
-       Student(int rollNo,String name,int marks){
-          this.rollNo=rollNo;
-          this.name=name;
-          this.marks=marks;
-      }
-      public String toString(){
-           return "RollNo: "+rollNo+" "+"Name: "+name+" "+"Marks: "+marks;
-      }
-   }
  
